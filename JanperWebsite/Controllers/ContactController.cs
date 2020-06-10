2ece1149dced7fb1fe1fc16ce4d5316168bba34c
Jason Ho <Jason.Ho@safecode.com.au>
2020-06-11 08:48:13 +1000

    Add reception email

--- JanperWebsite/Controllers/ContactController.cs
+++ JanperWebsite/Controllers/ContactController.cs
@@ -1,23 +1,24 @@
 ﻿using JanperWebsite.Models;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace IdacsPLUS.Controllers
 {
     public class EnquiriesController : Controller
     {
-        //public ActionResult Index()
-        //{
-        //    return View(new Model_Contact());
-        //}
+        private readonly string _systemEmail = ConfigurationManager.AppSettings["infoMail"];
+        private readonly string _receptionEmail = ConfigurationManager.AppSettings["ReceptionEmail"];
+        private readonly SmtpClient _smtpClient;
+
+        public EnquiriesController()
+        {
+            _smtpClient = new SmtpClient(ConfigurationManager.AppSettings["hostName"]);
+        }
 
         [ChildActionOnly]
         public ActionResult _Enquiries()
@@ -34,7 +35,7 @@ namespace IdacsPLUS.Controllers
         private bool IsHuman()
         {
             var response = Request["g-recaptcha-response"];
-            string secretKey = "6LdaXkAUAAAAAKkYNn1fynKmAI2MK4PRTmJ2lLHB";
+            const string secretKey = "6LdaXkAUAAAAAKkYNn1fynKmAI2MK4PRTmJ2lLHB";
 
             var client = new WebClient();
             var result =
@@ -52,62 +53,17 @@ namespace IdacsPLUS.Controllers
         {
             if (ModelState.IsValid && IsHuman())
             {
-                string infoMail = ConfigurationManager.AppSettings["infoMail"].ToString();
-                string emailTemplateConfirmation = ConfigurationManager.AppSettings["templateConfirmation"].ToString();
-                string emailTemplateNotification = ConfigurationManager.AppSettings["templateNotification"].ToString();
-                string mailBody = string.Empty;
-                string Name = model.Name;
-                string PhoneNumber = model.Phone;
-                string EmailAddress = model.Email;
-                string AdditionalComment = model.AdditionalComments;
-                //bool PrivacyAccepted = model.TermsAndConditions;
                 try
                 {
                     //Email to customer
-                    mailBody = System.IO.File.ReadAllText(Server.MapPath(emailTemplateConfirmation));
-                    mailBody = mailBody.Replace("??FIRSTNAME??", Name);
+                    await EmailToCustomerAsync(model);
 
-                    using (MailMessage mailObjConfirmation = new MailMessage())
-                    {
-                        mailObjConfirmation.From = new MailAddress(infoMail);
-                        mailObjConfirmation.To.Add(new MailAddress(EmailAddress));
-                        mailObjConfirmation.Subject = "Your enquiry on the Janper website";
-                        mailObjConfirmation.IsBodyHtml = true;
-                        mailObjConfirmation.Body = mailBody;
-                        mailObjConfirmation.Sender = new MailAddress(infoMail);
-                        SmtpClient SMTPServerConfirmation = new SmtpClient();
-                        SMTPServerConfirmation.Host = ConfigurationManager.AppSettings["hostName"].ToString();
-                        SMTPServerConfirmation.Send(mailObjConfirmation);
-                    }
                     //email to staff
-                    mailBody = System.IO.File.ReadAllText(Server.MapPath(emailTemplateNotification));
-                    mailBody = mailBody.Replace("??FIRSTNAME??", Name);
-                    mailBody = mailBody.Replace("??EMAIL??", EmailAddress);
-                    mailBody = mailBody.Replace("??PHONE??", PhoneNumber);
-
-                    mailBody = mailBody.Replace("??COMMENTS??", AdditionalComment);
-
-
-
-                    using (MailMessage mailObjConfirmation = new MailMessage())
-                    {
-                        mailObjConfirmation.From = new MailAddress(infoMail);
-                        mailObjConfirmation.To.Add(new MailAddress(infoMail));
-                        mailObjConfirmation.Subject = "New enquiry from the Janper website";
-                        mailObjConfirmation.IsBodyHtml = true;
-                        mailObjConfirmation.Body = mailBody;
-
-                        SmtpClient SMTPServerConfirmation = new SmtpClient();
-                        SMTPServerConfirmation.Host = ConfigurationManager.AppSettings["hostName"].ToString();
-                        SMTPServerConfirmation.Send(mailObjConfirmation);
-                        //model.successful = true;
-                    }
-                    //ModelState.AddModelError("Result", StringEnum.GetStringValue(ResponseActivityEnum.Success));
+                    await EmailToStaffAsync(model);
                 }
                 catch (Exception ex)
                 {
-                    string errorResponse = ex.Message.ToString();
-                    //ModelState.AddModelError("Result", StringEnum.GetStringValue(ResponseActivityEnum.Error));
+                    throw new Exception(ex.Message);
                 }
 
                 return PartialView("_Enquiries", model);
@@ -117,6 +73,40 @@ namespace IdacsPLUS.Controllers
                 return PartialView("_Enquiries", model);
             }
 
+        }
+
+        private async Task EmailToCustomerAsync(Model_Contact model)
+        {
+            var mailBody = System.IO.File.ReadAllText(Server.MapPath(ConfigurationManager.AppSettings["templateConfirmation"]));
+            mailBody = mailBody.Replace("??FIRSTNAME??", model.Name);
+
+            using (var message = new MailMessage(_systemEmail, model.Email))
+            {
+                message.Sender = new MailAddress(_systemEmail);
+                message.Subject = "Your enquiry on the Janper website";
+                message.IsBodyHtml = true;
+                message.Body = mailBody;
+                await _smtpClient.SendMailAsync(message);
+            }
+        }
+
+        private async Task EmailToStaffAsync(Model_Contact model)
+        {
+            var mailBody = System.IO.File.ReadAllText(Server.MapPath(ConfigurationManager.AppSettings["templateNotification"]));
+            mailBody = mailBody.Replace("??FIRSTNAME??", model.Name);
+            mailBody = mailBody.Replace("??EMAIL??", model.Email);
+            mailBody = mailBody.Replace("??PHONE??", model.Phone);
+            mailBody = mailBody.Replace("??COMMENTS??", model.AdditionalComments);
+
+
+            using (var message = new MailMessage(_systemEmail, _receptionEmail))
+            {
+                message.Subject = "New enquiry from the Janper website";
+                message.IsBodyHtml = true;
+                message.Body = mailBody;
+
+                await _smtpClient.SendMailAsync(message);
+            }
         }
     }
 }
